f295af87bbb6d029e30a9c0a3125a5df0d18f5fc
ntsoa1903mana <140753155+ntsoa1903mana@users.noreply.github.com>
2023-10-12 12:45:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,47 +3,70 @@ from pydantic import BaseModel
 import openai
 from dotenv import load_dotenv
 import os
+import random
 
 # Load environment variables from the .env file
 load_dotenv()
 
 app = FastAPI()
 
-# Get the OpenAI API key from the environment variables
-api_key = os.getenv("OPENAI_API_KEY")
+## Store multiple OpenAI API keys in a list
+api_keys = [
+    "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",
+    "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",
+    "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",
+    # Add more API keys as needed
+    # Add more API keys as needed
+]
 
-# Set the OpenAI API key in the openai library
-openai.api_key = api_key
+# Shuffle the API keys at the beginning to ensure better randomness
+random.shuffle(api_keys)
 
 class PromptRequest(BaseModel):
     prompt: str
     fbid: str
 
-async def generate_response(fbid, prompt):
-    user_prompt = prompt  # Note: There's a syntax error in your code, extra quotation mark (")
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=user_prompt,
+@app.post("/generate_response")
+async def generate_response(prompt_request: PromptRequest):
+    user_prompt = prompt_request.prompt
+    fbid = prompt_request.fbid
+
+    # Choose the next API key in the shuffled list
+    selected_api_key = api_keys.pop(0)
+    api_keys.append(selected_api_key)  # Move the used key to the end of the list for rotation
+
+    # Set the selected API key in the openai library
+    openai.api_key = selected_api_key
+
+    # Print the selected API key
+    print(f"Selected API Key: {selected_api_key}")
+
+    # Define messages for conversation
+    messages = [
+        {"role": "system", "content": "anwer the question directly add small value"},
+        {"role": "user", "content": user_prompt},
+    ]
+
+    # Use OpenAI GPT-3 for chat completion
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
         temperature=0.7,
-        max_tokens=256,
+        max_tokens=500,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
         stop=["Human: ", "AI: "]
     )
-    return response.choices[0].text
 
-@app.post("/generate_response")
-async def handle_generate_response(prompt_request: PromptRequest):
-    user_prompt = prompt_request.prompt
-    fbid = prompt_request.fbid
-    response_text = await generate_response(fbid, user_prompt)
-    return {"fbid": fbid, "response": response_text}
+    response_content = chat_completion['choices'][0]['message']['content']
+
+    return {"fbid": fbid, "response": response_content}
 
 @app.get("/")
 async def home():
     print("Home endpoint reached")
-    return {"message": "openAI"}
+    return {"message": "OpenAI GPT-3 API"}
 
 if __name__ == "__main__":
     import uvicorn
