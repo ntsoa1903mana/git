00981a41769a8ff34398632da12012047efb84db
ntsoa1903mana <140753155+ntsoa1903mana@users.noreply.github.com>
2023-10-12 17:26:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import openai
 from dotenv import load_dotenv
@@ -19,31 +19,42 @@ class PromptRequest(BaseModel):
     prompt: str
     fbid: str
 
-async def generate_response(fbid, prompt):
-    user_prompt = prompt  # Note: There's a syntax error in your code, extra quotation mark (")
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="generate an answer with value and longer if needed: " + user_prompt ,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["Human: ", "AI: "]
-    )
-    return response.choices[0].text
-
 @app.post("/generate_response")
-async def handle_generate_response(prompt_request: PromptRequest):
+async def generate_response(prompt_request: PromptRequest):
     user_prompt = prompt_request.prompt
+    print(user_prompt)
     fbid = prompt_request.fbid
-    response_text = await generate_response(fbid, user_prompt)
-    return {"fbid": fbid, "response": response_text}
+
+    # Define messages for conversation
+    messages = [
+        {"role": "system", "content": "answer the question directlygive 1 exemple "},
+        {"role": "user", "content": user_prompt},
+    ]
+
+    try:
+        # Use OpenAI GPT-3 for chat completion
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.7,
+            max_tokens=600,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["Human: ", "AI: "]
+        )
+
+        response_content = chat_completion['choices'][0]['message']['content']
+        print(response_content)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error gen response")
+
+    return {"fbid": fbid, "response": response_content}
 
 @app.get("/")
 async def home():
     print("Home endpoint reached")
-    return {"message": "openAI"}
+    return {"message": "OpenAI GPT-3 API"}
 
 if __name__ == "__main__":
     import uvicorn
